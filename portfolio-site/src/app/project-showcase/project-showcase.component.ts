29bfd38bfa017a80029f586fbdb6eca97d09b88c
CodyScha <codscha@siue.edu>
2023-06-09 10:08:28 -0500

    these cards are not right, but they dont look bad

--- portfolio-site/src/app/project-showcase/project-showcase.component.ts
+++ portfolio-site/src/app/project-showcase/project-showcase.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-project-showcase',
-  templateUrl: './project-showcase.component.html',
-  styleUrls: ['./project-showcase.component.css']
+	selector: 'app-project-showcase',
+	templateUrl: './project-showcase.component.html',
+	styleUrls: ['./project-showcase.component.css']
 })
 export class ProjectShowcaseComponent {
+	projects: any = [];
 
+	constructor() {
+		this.projects = [
+			{
+				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
+			},
+			{
+				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/rinzler.webp", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
+			},
+			{
+				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github", secondLink: "twitch.tv/xqc", secondLinkIcon: "bi bi-twitch"
+			},
+			{
+				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
+			}
+		]
+	}
 }
