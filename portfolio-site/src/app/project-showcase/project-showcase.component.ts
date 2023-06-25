69868c390d947a730c4fc8e9d0abe4a0554e533f
CodyScha <codscha@siue.edu>
2023-06-25 16:51:24 -0500

    finished site card

--- portfolio-site/src/app/project-showcase/project-showcase.component.ts
+++ portfolio-site/src/app/project-showcase/project-showcase.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 
 @Component({
 	selector: 'app-project-showcase',
@@ -12,17 +11,17 @@ export class ProjectShowcaseComponent {
 	constructor() {
 		this.projects = [
 			{
-				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github", mainLinkTitle: "GitHub Repo"
+				name: "Portfolio Website", date: "June 2023", image: "../../..//src/assets/portfolio-site-image.png", description: "You're currently looking at this project! This project was made with Angular, and I am hosting it on GitHub Pages. My future plans are to expand this website with other features like games and backend features to learn more about web development.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github", mainLinkTitle: "Source Code"
 			},
-			{
-				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/rinzler.webp", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
-			},
-			{
-				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github", secondLink: "twitch.tv/xqc", secondLinkIcon: "bi bi-twitch"
-			},
-			{
-				name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
-			}
+			// {
+			// 	name: "Ingress Portal Mapping", date: "August 2022 - May 2023", image: "", description: "For my Senior Capstone project, my team and I created a solution for generating gameplay strategies for the popular mobile game Ingress. Using our JavaScript plugin, users can download official game data from the web and import it into our Flutter application Spoke-It to generate an interactive visual of their strategy. Users can edit the dataset in real time to see an updated strategy and points calculation and save them as profiles for future viewing.", mainLink: "https://github.com/CodyScha/Spoke-It", mainLinkIcon: "bi bi-github", mainLinkTitle: "Spoke-It Repository", secondLink: "https://github.com/CodyScha/Ingress-Intel-Map-Portal-Scraper", secondLinkIcon: "bi bi-github", secondLinkTitle: "Portal Scraper Repository"
+			// },
+			// {
+			// 	name: "MCU Character Interactions Network", date: "April 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github", secondLink: "twitch.tv/xqc", secondLinkIcon: "bi bi-twitch"
+			// },
+			// {
+			// 	name: "Portfolio Website", date: "May 2023", image: "../../..//src/assets/cute-pig-polite-pig.gif", description: "This is a project desc.", mainLink: "https://github.com/CodyScha/codyscha.github.io", mainLinkIcon: "bi bi-github"
+			// }
 		]
 	}
 }
